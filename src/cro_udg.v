983765f0fa048b24af01de821006320cb2a03cc0
Carlos Andrés Guerra <85963142+CarlosGS99@users.noreply.github.com>
2023-11-04 00:43:05 -0600

    Update cro_udg.v

--- src/cro_udg.v
+++ src/cro_udg.v
@@ -1,4 +1,4 @@
-module cro_udg (
+module tt_um_carlosgs99_cro_udg (
     input  wire [7:0] ui_in,  // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out, // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in, // IOs: Bidirectional Input path
